3f0590445d07ddf2c06b92dc84b99fd226c77ac1
Nick Doiron <ndoiron@mapmeld.com>
2016-09-27 23:07:00 -0400

    handlebars and static files

--- src/hello.rs
+++ src/hello.rs
@@ -1,11 +1,20 @@
 extern crate iron;
 extern crate router;
+extern crate staticfile;
+extern crate mount;
+extern crate handlebars_iron as hbs;
 
 use std::str::FromStr;
 use std::env;
-use iron::{Iron, Request, Response, IronResult};
+use iron::prelude::*;
 use router::Router;
 use iron::status;
+use std::path::Path;
+use staticfile::Static;
+use mount::Mount;
+use hbs::{Template, HandlebarsEngine, DirectorySource, MemorySource};
+use std::collections::BTreeMap;
+use std::sync::Arc;
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
@@ -17,7 +26,11 @@ fn hello(_: &mut Request) -> IronResult<Response> {
 fn hello_name(req: &mut Request) -> IronResult<Response> {
     let params = req.extensions.get::<Router>().unwrap();
     let name = params.find("name").unwrap();
-    let resp = Response::with((status::Ok, format!("Hello, {}!", name)));
+
+    let mut resp = Response::new();
+    let mut datatree = BTreeMap::new();
+    datatree.insert("name".to_string(), name.to_string());
+    resp.set_mut(Template::new("hello", datatree)).set_mut(status::Ok);
     Ok(resp)
 }
 
@@ -28,12 +41,32 @@ fn get_server_port() -> u16 {
 }
 
 /// Configure and run our server.
+// based on https://medium.com/@ericdreichert/rusts-iron-framework-getting-started-b90977811d8c
 fn main() {
+    // set up handlebars views
+    let mut hbse = HandlebarsEngine::new();
+    let views_path = "./views/";
+    hbse.add(Box::new(DirectorySource::new(views_path, ".hbs")));
+    if let Err(r) = hbse.reload() {
+        panic!("views failed");
+    }
+    let hbse_ref = Arc::new(hbse);
+    // hbse_ref.watch(views_path);
+
+    // set up Chain for hello_name
+    let mut hello_chain = Chain::new(hello_name);
+    hello_chain.link_after(hbse_ref);
+
     // Set up our URL router.
     let mut router: Router = Router::new();
     router.get("/", hello, "index");
-    router.get("/:name", hello_name, "name");
+    router.get("/name/:name", hello_chain, "name");
+
+    // static directory
+    let mut mount = Mount::new();
+    mount.mount("/", router)
+         .mount("/static/", Static::new(Path::new("./public/")));
 
     // Run the server.
-    Iron::new(router).http(("0.0.0.0", get_server_port())).unwrap();
+    Iron::new(mount).http(("0.0.0.0", get_server_port())).unwrap();
 }
